8e464f962d1c4d072ab5462734eb7480b79b01d2
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-04-18 22:53:26 +0200

    chore: Adding Project Required Dependencies (Spring-Web,Lombok,Spring-Data-Jpa,MySQL)

---
+++ src/main/java/com/youssef/gamal/library_magement_system_app/LibraryManagementSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.library_magement_system_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youssef/gamal/library_magement_system_app/LibraryManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.library_magement_system_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
