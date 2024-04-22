7bafd6428f8769c8890b22ffd997c0640a7e99bd
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-04-22 12:18:39 +0200

    test: Added tests for Integration-Testing for BorrowingController in BorrowingControllerTests.java for POST /v1/borrow/{bookId}/patron/{patronId}

---
+++ src/test/java/com/youssef/gamal/library_magement_system_app/borrowing/BorrowingControllerTests.java
@@ -0,0 +1,94 @@
+package com.youssef.gamal.library_magement_system_app.borrowing;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.NoSuchElementException;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = BorrowingController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class BorrowingControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BorrowingServiceInterface borrowingServiceInterface;
+
+    @Test
+    void borrowBook_shouldReturnOk_whenBookAndPatronExists() throws Exception {
+
+
+        // Arrange
+        Long bookId = 1L;
+        Long patronId = 2L;
+        LocalDate dateMustReturnIn = LocalDate.now().plusDays(3);
+        BorrowingRequestBody requestBody = BorrowingRequestBody.builder()
+                .dateMustReturnIn(dateMustReturnIn)
+                .build();
+
+        // Act
+        borrowingServiceInterface.borrowBook(bookId, patronId, dateMustReturnIn);
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // to handle java time module in ObjectMapper
+        ResultActions result = mockMvc.perform(post("/v1/borrow/{bookId}/patron/{patronId}", bookId, patronId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(requestBody)));
+
+        // Assert
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    void borrowBook_shouldReturnBadRequest_whenBookOrPatronNotFound() throws Exception {
+        // Arrange
+        Long bookId = 1L;
+        Long patronId = 2L;
+        LocalDate dateMustReturnIn = LocalDate.now().plusDays(3);
+        BorrowingRequestBody requestBody = new BorrowingRequestBody(dateMustReturnIn);
+
+
+        doThrow(NoSuchElementException.class)
+                .when(borrowingServiceInterface)
+                .borrowBook(any(), any(), any());
+
+
+        // Act
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // to handle java time module in ObjectMapper
+        ResultActions result = mockMvc.perform(post("/v1/borrow/{bookId}/patron/{patronId}", bookId, patronId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(requestBody)));
+
+        // Assert
+        result.andExpect(status().isNotFound());
+    }
+}
