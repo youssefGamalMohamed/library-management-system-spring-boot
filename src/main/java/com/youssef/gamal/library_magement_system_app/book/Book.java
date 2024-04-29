a1e263a7c21229124d82f26868e567e8251b6cc4
Youssef Gamal <youssef.gamal@intercom.com.eg>
2024-04-29 16:37:54 +0300

    fix: remove @Data and replace it with seperated annotations & added @JsonIgnore to prevent StackOverFlow

--- src/main/java/com/youssef/gamal/library_magement_system_app/book/Book.java
+++ src/main/java/com/youssef/gamal/library_magement_system_app/book/Book.java
@@ -1,21 +1,24 @@
 package com.youssef.gamal.library_magement_system_app.book;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.youssef.gamal.library_magement_system_app.borrowing.Borrowing;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.io.Serializable;
 import java.time.Year;
 import java.util.List;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
+public class Book implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,5 +35,23 @@ public class Book {
     private String author;
 
     @OneToMany(mappedBy = "book" , cascade = CascadeType.ALL)
+    @ToString.Exclude
+    @JsonIgnore // to avoid stack-overflow while retrieve borrowings with fasterxml.jackson
     private List<Borrowing> borrowings;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Book book = (Book) o;
+        return getId() != null && Objects.equals(getId(), book.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
